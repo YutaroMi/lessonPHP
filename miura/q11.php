f460f3cb53b1d121b100fc5415905576e348c8f1
YutaroMi <yodokame@outlook.jp>
2018-12-12 15:43:08 +0900

    10分くらい

---
+++ miura/q11.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Q11</title>
+</head>
+<body>
+	<?php  
+	
+	function getSumNumber($num){
+		
+		return $num*1.08;
+
+	}
+	echo getSumNumber(10000);
+
+	
+
+	?>
+
+</body>
+</html>
